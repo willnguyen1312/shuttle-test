8a7d762b06d85292fb9bfc5392d09fb2679da9d6
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-12-01 10:40:37 -0700

    💅

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,3 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
 
     Ok(config.into())
 }
-
-
-// https://playhop.com/app/272602?utm_campaign=en-tier_rich_playhop_title-top_google_search%257C21174017225&utm_medium=search&utm_source=game_header_logo&utm_term=fish%2520eat%2520getting%2520big&utm_content=k50id%7Ckwd-2173349715979%7Ccid%7C21174017225%7Caid%7C696263462870%7Cgid%7C162304046873%7Cpos%7C%7Csrc%7Cg_%7Cdvc%7Cc%7Creg%7C9215891%7Crin%7C%7C#goog_rewarded
